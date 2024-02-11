7546635106e3e26a9b8a88e4ab18beb30595b95f
RamdlanFaqih <ramfaqih@gmail.com>
2024-02-11 18:44:54 +0700

    rn-jobs

---
+++ app/_layout.js
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router/stack";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+export default function Layout() {
+  const [fontLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayourRootView = useCallback(async () => {
+    if(fontLoaded) {
+        await SplashScreen.hideAsync();
+    }
+  }, [fontLoaded])
+
+  if(!fontLoaded) return null;
+  
+  return <Stack onLayout={onLayourRootView} />;
+}
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/faqih.jpg";
+
+export default {
+  profile,
+};
